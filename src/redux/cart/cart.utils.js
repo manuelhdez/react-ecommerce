519f0e2ec5d830b9df672671edc7c63da0645a0b
Manuel Hernandez <ing.merhandez@gmail.com>
2019-09-24 20:21:14 -0600

    added arrow funcionality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const exists = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (exists.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItemToRemove, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};

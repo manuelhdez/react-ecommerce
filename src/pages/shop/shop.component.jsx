006a73e6a5d21b2fed72ce0b7cc3df03ffd3a9d2
Manuel Hernandez <ing.merhandez@gmail.com>
2019-09-25 21:24:02 -0600

    shop, collection overview & directory

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,15 @@
 import React from "react";
 
-import SHOP_DATA from "./shop.data";
+import CollectionsOverview from "./../../components/collections-overview/collections-overview.component";
 
-import CollectionPreview from "./../../components/collection-preview/collection-preview.component";
+const ShopPage = ({ collections }) => {
+  console.log(collections);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => {
-          return <CollectionPreview key={id} {...otherCollectionProps} />;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
